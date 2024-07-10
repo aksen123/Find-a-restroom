d094c9d4707aca003b627352ebbebafec92a9607
mw <minwook0523@gmail.com>
2024-07-10 19:02:14 +0900

    인트로페이지 퍼블리싱 (애니메이션 X)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import KakaoLoader from "./components/kakao-loader";
 import { Map } from "react-kakao-maps-sdk";
 import Script from "next/script";
 import MainMap from "./components/Map";
+import IntroPage from "./components/IntroPage";
 
 export default function Page() {
   return (
     <>
-      <MainMap />
+      <IntroPage />
+      {/* <MainMap /> */}
     </>
   );
 }
