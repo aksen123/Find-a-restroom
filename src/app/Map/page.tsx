28711f3b8069d0a69705502d6df6add0233af350
mw <minwook0523@gmail.com>
2024-07-30 15:57:03 +0900

    드래그, 줌 변경시 해당범위 마커표시, 지도레벨 5이상시 마커 클러스터 추가

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -13,6 +13,9 @@ export default function Page() {
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
   const [location, setLocation] = useState<Location | null>(null);
   const [markers, setMarkers] = useState<kakao.maps.Marker[] | null>(null);
+  const [markerClusterer, setMarkerClusterer] = useState<
+    kakao.maps.MarkerClusterer[] | null
+  >(null);
   const [loading, error] = useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_KAKAO_KEY as string,
     libraries: ["clusterer", "drawing", "services"],
@@ -36,9 +39,9 @@ export default function Page() {
   useEffect(() => {
     if (location && map) {
       getRestroom(map);
-      console.log(map, "변경!!");
     }
   }, [location, map]);
+
   const getRestroom = async (map: kakao.maps.Map) => {
     const bounds = map.getBounds();
     const params = {
@@ -48,7 +51,7 @@ export default function Page() {
       ne_lng: bounds.getNorthEast().getLng(),
     };
     const restrooms = await getMarker.get(params as Param);
-    const markers = restrooms.map((el) => {
+    const newMarkers = restrooms.map((el) => {
       const marker = new kakao.maps.Marker({
         position: new kakao.maps.LatLng(el.latitude, el.longitude),
         title: el.toilet_name,
@@ -57,10 +60,40 @@ export default function Page() {
 
       return marker;
     });
-    // markers.map((el) => el.setMap(null));
-    setMarkers(markers);
+    const cluster = new kakao.maps.MarkerClusterer({
+      map: map,
+      minLevel: 5,
+    });
+
+    cluster.clear();
+    cluster.addMarkers(newMarkers);
+    markers?.forEach((el) => el.setMap(null));
+    setMarkers(newMarkers);
     console.log(restrooms);
   };
+  const changeMap = () => {
+    if (map) {
+      getRestroom(map);
+    }
+  };
+  const myLocationClick = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        if (map) {
+          const newCenter = new kakao.maps.LatLng(
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          map.setCenter(newCenter);
+          getRestroom(map);
+        }
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+  };
+
   return (
     <>
       <div className="relative">
@@ -69,9 +102,26 @@ export default function Page() {
             center={location}
             style={{ width: "100%", height: "100vh", position: "relative" }}
             level={3}
+            onDragEnd={changeMap}
+            onZoomChanged={changeMap}
             onCreate={(map) => setMap(map)}
           ></Map>
         )}
+        <div className="z-30 absolute top-6 left-6">
+          <span>메뉴바</span>
+        </div>
+        <div onClick={myLocationClick} className="z-30 absolute top-6 right-6">
+          <span>내 위치</span>
+        </div>
+        <div className="absolute bottom-0 bg-slate-500 w-full h-20 z-30 rounded-t-3xl flex items-center justify-center">
+          <form
+            action=""
+            className="relative w-4/5 h-1/2 bg-white rounded-2xl overflow-hidden flex items-center px-1"
+          >
+            <input type="text" className="w-full h-full outline-none p-3" />
+            <button className="border-2 w-8 h-8 border-blue-700 rounded-2xl"></button>
+          </form>
+        </div>
       </div>
     </>
   );
