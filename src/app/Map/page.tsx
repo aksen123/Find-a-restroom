d53164efd72d8fc211965bc4479d01480ee8e28f
mw <minwook0523@gmail.com>
2024-08-10 08:26:10 +0900

    경로데이터 타입지정

--- src/app/Map/page.tsx
+++ src/app/Map/page.tsx
@@ -171,10 +171,10 @@ export default function Page() {
             {polyline && (
               <Polyline
                 path={polyline}
-                strokeWeight={3} // 선의 두께입니다
-                strokeColor={"#db4040"} // 선의 색깔입니다
-                strokeOpacity={1} // 선의 불투명도입니다 0에서 1 사이값이며 0에 가까울수록 투명합니다
-                strokeStyle={"solid"} // 선의 스타일입니다
+                strokeWeight={3}
+                strokeColor={"#db4040"}
+                strokeOpacity={1}
+                strokeStyle={"solid"}
               />
             )}
             {overlay && (
--- src/app/api/path/route.ts
+++ src/app/api/path/route.ts
@@ -2,6 +2,76 @@ import api from "@/app/service/axios";
 import axios from "axios";
 import { NextRequest } from "next/server";
 
+// 인터페이스 정의
+interface Location {
+  name: string;
+  x: number;
+  y: number;
+}
+
+interface Bound {
+  min_x: number;
+  min_y: number;
+  max_x: number;
+  max_y: number;
+}
+
+interface Fare {
+  taxi: number;
+  toll: number;
+}
+
+interface Summary {
+  origin: Location;
+  destination: Location;
+  waypoints: Location[];
+  priority: string;
+  bound: Bound;
+  fare: Fare;
+  distance: number;
+  duration: number;
+}
+
+interface Road {
+  name: string;
+  distance: number;
+  duration: number;
+  traffic_speed: number;
+  traffic_state: number;
+  vertexes: number[];
+}
+
+interface Guide {
+  name: string;
+  x: number;
+  y: number;
+  distance: number;
+  duration: number;
+  type: number;
+  guidance: string;
+  road_index: number;
+}
+
+interface Section {
+  distance: number;
+  duration: number;
+  bound: Bound;
+  roads: Road[];
+  guides: Guide[];
+}
+
+interface Route {
+  result_code: number;
+  result_msg: string;
+  summary: Summary;
+  sections: Section[];
+}
+
+interface NavigationData {
+  trans_id: string;
+  routes: Route[];
+}
+
 // TODO : 경로 갖고오기 완료
 // path만 정리해서 반환값으로 넘겨주고 폴리라인 path로 넣어주기
 
@@ -44,7 +114,20 @@ export async function GET(req: NextRequest) {
     headers: headers,
   });
 
-  const data = response.data ? response.data : null;
-  console.log(data.routes[0].sections[0].roads);
+  const data: NavigationData = response.data ? response.data : null;
+  const linePath: kakao.maps.LatLng[] = [];
+  data.routes[0].sections[0].roads.forEach((router) => {
+    router.vertexes.forEach((vertex, index) => {
+      if (index % 2 === 0) {
+        linePath.push(
+          new kakao.maps.LatLng(
+            router.vertexes[index + 1],
+            router.vertexes[index]
+          )
+        );
+      }
+    });
+  });
+  console.log(linePath);
   return Response.json({ data: 1 });
 }
